001baddeacfc1ee52173faf64a29978d20ebc561
Gismo Ranas <cataldomanigrasso@gmail.com>
2014-02-03 17:51:23 +0100

    added pair class

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -r `find /home/cataldo/Workspace/dev/dwm/suite/network/trunk/network-model/ \( -name Utils.java -o -name LambdaUtils.java -o -name Matchers.java \)` .
+rsync -r `find /home/cataldo/Workspace/dev/dwm/suite/network/trunk/network-model/ \( -name Utils.java -o -name LambdaUtils.java -o -name Matchers.java -o -name Pair.java \)` .
